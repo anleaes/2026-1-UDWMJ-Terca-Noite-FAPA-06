ae5d784be6c9394a0a2d594f6d246e831fc07ae2
pietrosdv <162866897+pietrosdv@users.noreply.github.com>
2026-05-26 18:39:15 -0300

    Update model.py

--- apps/animal/model.py
+++ apps/animal/model.py
@@ -2,13 +2,13 @@ from django.db import models
 
 class Animal(models.Model):
     nome = models.CharField('Nome', max_length=50)
-    especie = models.ForeignKey(Especie, on_delete=models.CASCADE)
+    foto = models.ImageField('Foto', upload_to='photos')
+    especie = models.CharField(Especie, max_length=50)
     raca = models.CharField('Raça', max_length=100)
-    sexo = models.BooleanField('Sexo', default=False)
+    sexo = models.CharField('Sexo', max_length=1, choices=[('M','Masculino'),('F','Feminino')])
     descricao = models.TextField('Descricao', max_length=200)
     data_inscricao = models.DateField('Data de inscricao', auto_now=False, auto_now_add=False)
     disponivel = models.BooleanField('Disponivel', default=False)
-    foto = models.ImageField('Foto', upload_to='photos')
     
     
     class Meta:
